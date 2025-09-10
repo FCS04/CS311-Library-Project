55015c3413cf3f67502d2c25882a37cca142c7ef
admin <admin@DESKTOP-I4AVUKS>
2025-09-11 00:14:15 +0800

    Added a button and codes

--- Library-Project/frmlogin.Designer.cs
+++ Library-Project/frmlogin.Designer.cs
@@ -30,9 +30,11 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
             this.chkshow = new System.Windows.Forms.CheckBox();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -53,19 +55,20 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "Password:";
             // 
-            // textBox1
+            // txtusername
             // 
-            this.textBox1.Location = new System.Drawing.Point(161, 75);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(185, 20);
-            this.textBox1.TabIndex = 2;
+            this.txtusername.Location = new System.Drawing.Point(161, 75);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(185, 20);
+            this.txtusername.TabIndex = 2;
             // 
-            // textBox2
+            // txtpassword
             // 
-            this.textBox2.Location = new System.Drawing.Point(161, 114);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(185, 20);
-            this.textBox2.TabIndex = 3;
+            this.txtpassword.Location = new System.Drawing.Point(161, 114);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(185, 20);
+            this.txtpassword.TabIndex = 3;
+            this.txtpassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtpassword_KeyPress);
             // 
             // chkshow
             // 
@@ -75,17 +78,42 @@
             this.chkshow.Name = "chkshow";
             this.chkshow.Size = new System.Drawing.Size(102, 17);
             this.chkshow.TabIndex = 4;
-            this.chkshow.Text = "Show Password";
+            this.chkshow.Text = "&Show Password";
             this.chkshow.UseVisualStyleBackColor = true;
+            this.chkshow.CheckedChanged += new System.EventHandler(this.chkshow_CheckedChanged);
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnlogin.Location = new System.Drawing.Point(161, 180);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(79, 23);
+            this.btnlogin.TabIndex = 5;
+            this.btnlogin.Text = "&Login";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // btnclear
+            // 
+            this.btnclear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnclear.Location = new System.Drawing.Point(278, 179);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(75, 23);
+            this.btnclear.TabIndex = 6;
+            this.btnclear.Text = "&Clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
             // 
             // frmlogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(437, 224);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnlogin);
             this.Controls.Add(this.chkshow);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtusername);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
@@ -102,8 +130,10 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.TextBox txtpassword;
         private System.Windows.Forms.CheckBox chkshow;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.Button btnclear;
     }
 }
